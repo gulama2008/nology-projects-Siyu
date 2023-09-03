0f8bf1dba4bedbde5394bd66cbd324f4d5be6337
Siyu Liu <siyu.java.web@gmail.com>
2023-09-03 11:02:02 +1000

    adds menu togger in js

--- portfolio-Siyu/script.js
+++ portfolio-Siyu/script.js
@@ -1,4 +1,24 @@
+const menuIcon = document.querySelector(".navbar__menu__hb");
+const menu = document.querySelector('.navbar__menu__list--sm');
+const body = document.querySelector('body');
 const sections = document.querySelectorAll("section");
+let isMenuOpen = false;
+
+function clickOnMenuIcon() { 
+  isMenuOpen = !isMenuOpen;
+  if (isMenuOpen) {
+    menu.style.display = 'block';
+  } else { 
+    menu.style.display = 'none';
+  }
+}
+menuIcon.addEventListener('click',clickOnMenuIcon) 
+
+// function clickOnPage() { 
+//   menu.style.display = 'none';
+// }
+// body.addEventListener('click', clickOnPage);
+
 sections.forEach((section) => { 
     section.classList.add('loadin')
 })
